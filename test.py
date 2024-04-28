9fe115bc5273dce3fecffaaff1a23c5d99086b49
sakusaku <oreosagi874@gmail.com>
2024-04-28 21:20:43 +0900

    add

---
+++ test.py
@@ -0,0 +1,35 @@
+
+class A:
+    @classmethod
+    def show(cls):
+        print(
+            cls.__name__
+        )
+
+class B(A):
+    pass
+
+class C(B):
+    pass
+
+
+def subclasses(cls)->type:
+    yield cls
+    for subclass in cls.__subclasses__():
+        yield from subclasses(subclass)
+
+
+
+
+for cls in subclasses(A):
+    cls.show()
+
+
+gen = subclasses(A)
+
+gen.__next__()
+
+for sub in gen:
+    sub.show()
+    
+    
